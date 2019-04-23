9e24f2f57862ece1118d6111586e973730954ae2
wjh <251248963@qq.com>
2019-04-23 19:07:05 +0800

    点赞事件只绑定内容页

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,13 +1,15 @@
 (function(){
 
-    document.getElementById("like_btn").addEventListener("click", function(){
-        fetch("/api/like/"+window.location.href.split("/")[window.location.href.split("/").length - 1], {
-            method: "GET"
+    if(document.getElementById("like_btn")) {
+        document.getElementById("like_btn").addEventListener("click", function(){
+            fetch("/api/like/"+window.location.href.split("/")[window.location.href.split("/").length - 1], {
+                method: "GET"
+            })
+            .then(res=>res.json())
+            .then(res=>{
+                console.log(res);
+                document.getElementById("like").innerText = res.data;
+            })
         })
-        .then(res=>res.json())
-        .then(res=>{
-            console.log(res);
-            document.getElementById("like").innerText = res.data;
-        })
-    })
+    }
 })()
