11df9878032537f8bc8fa01bf9f5fbfb92857001
wjh <251248963@qq.com>
2019-05-11 18:00:02 +0800

    评论功能推进

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,8 +1,23 @@
 (function(){
-
+    //点赞
     if(document.getElementById("like_btn")) {
         document.getElementById("like_btn").addEventListener("click", function(){
             fetch("/api/like/"+window.location.href.split("/")[window.location.href.split("/").length - 1], {
+                method: "GET"
+            })
+            .then(res=>res.json())
+            .then(res=>{
+                console.log(res);
+                document.getElementById("like").innerText = res.data;
+            })
+        })
+    }
+
+
+    //
+    if(document.getElementById("commentBtn")) {
+        document.getElementById("commentBtn").addEventListener("click", function(){
+            fetch("/api/comment/"+window.location.href.split("/")[window.location.href.split("/").length - 1], {
                 method: "GET"
             })
             .then(res=>res.json())
